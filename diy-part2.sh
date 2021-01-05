67fff157d7b0d92f22258dc2f2f5bd5f84dcaf0f
lsydy1021 <59816384+lsydy1021@users.noreply.github.com>
2021-01-05 18:44:23 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,3 +13,4 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 git clone -b master https://github.com/vernesong/OpenClash.git package/luci-app-openclash  #openclash出国软件
+git clone https://github.com/Lienol/openwrt-luci/tree/18.06/themes/luci-theme-bootstrap-mod package/lean/luci-theme-bootstrap-mod #bootstrap-mod主题
