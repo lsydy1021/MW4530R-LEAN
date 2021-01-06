63f4f97a82cc1bddac2d5bfdfb0abfdda8da7a42
lsydy1021 <59816384+lsydy1021@users.noreply.github.com>
2021-01-06 09:14:33 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,3 +14,5 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 git clone https://github.com/kenzok8/openwrt-packages.git
 git clone https://github.com/kenzok8/small.git
+git clone https://github.com/fw876/helloworld.git
+git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns  #smartdns
